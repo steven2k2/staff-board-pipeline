13f6a6de0f81329c04485deb01b4733753d7a436
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-06 20:10:49 +1100

    Testing deploy.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-      ? '/staff-board-pipeline/'
-      : '/staff-board-pipeline/staging/',
+  base: '/staff-board-pipeline/',
   build: {
     outDir: 'dist',
   },
