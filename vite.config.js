1146b2d87cc61bcaec331b161f250d6315c33392
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-06 20:08:23 +1100

    chore: prepare main branch for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/staff-board-pipeline/staging/', // Ensure this matches the staging URL
+  base: process.env.NODE_ENV === 'production'
+      ? '/staff-board-pipeline/'
+      : '/staff-board-pipeline/staging/',
   build: {
     outDir: 'dist',
   },
+  plugins: [react()],
 });
