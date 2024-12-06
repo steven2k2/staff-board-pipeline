e0c334d87fa52a7e6cab188d492b739482a0c82b
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-06 19:26:49 +1100

    Testing deploy.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/staff-board-pipeline/',
+  base: '/staff-board-pipeline/staging/', // Ensure this matches the staging URL
   build: {
     outDir: 'dist',
   },
